160879cff54a10b9c8a095ff820d55de51c34dc6
Adrian Leip <adrian@leip.net>
2019-01-30 18:05:38 +0100

    start function retrieve fortra_x files for capmod runs

--- capmod_extract.r
+++ capmod_extract.r
@@ -1,3 +1,38 @@
+loadcapmod <- function(){
+  
+  
+  # Default batchdir location
+  batchout <- paste0(datapath, "../batchout")
+  batchdirs <- file.info(list.files(batchout, full.names=TRUE))
+  batchdirs <- batchdirs[with(batchdirs, order(as.POSIXct(mtime),decreasing=TRUE)), ]
+  batchdir <- rownames(batchdirs)[1]
+  
+  # Check fortra_X files
+  fortran <- list.files(batchdir, pattern = "fortra_[0-9]*.gms")
+  
+  # Extract setglobals
+  x<-1
+  con <- file(paste0(batchdir, "/", fortran[x]), open = "r")
+  setglobal <- readLines(con)
+  setglobal <- setglobal[grepl("setglobal", setglobal, ignore.case=TRUE)]
+  a <- strsplit(setglobal[86], " ")[[1]]
+  a <- c(a[2], paste(a[3:length(a)], collapse=" "))
+  
+  
+  
+  startextract('capmod')
+  fls <- getfilesfromfolder(curfol = paste0("\\\\s-jrciprap246p.jrc.it/dev/CAPRImodel/users/leipadr/results/capmod/20190125_mititech"), 
+                            flag = 'mititech', 
+                            reference="res_2_0830mtr_rd_refON.gdx"
+                            )
+  
+  
+  
+  
+}
+
+
+
 extractlca <- function(fls = NULL, curcountries = "", curyears = '30', baseyear = '08', curscenshort = NULL){
   
   curcols <- c('PROD', 'YILD')
